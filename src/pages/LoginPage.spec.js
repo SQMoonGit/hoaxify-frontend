92a4a72da2fed643e66fd2bd4dcce7136750e331
SQMoonGit <hykmoon@gmail.com>
2021-07-12 12:01:58 -0500

    Added click interactions in login page

--- src/pages/LoginPage.spec.js
+++ src/pages/LoginPage.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import { LoginPage } from "./LoginPage";
 
 describe("LoginPage", () => {
@@ -36,12 +36,27 @@ describe("LoginPage", () => {
   });
 
   describe("Interactions", () => {
+    let usernameInput, passwordInput, button;
+
     const changeEvent = (content) => {
       return {
         target: {
           value: content,
         },
       };
+    };
+
+    const setupForSubmit = (props) => {
+      const rendered = render(<LoginPage {...props}></LoginPage>);
+
+      const { container, queryByPlaceholderText } = rendered;
+      usernameInput = queryByPlaceholderText("Your username");
+      fireEvent.change(usernameInput, changeEvent("my-user-name"));
+      passwordInput = queryByPlaceholderText("Your password");
+      fireEvent.change(passwordInput, changeEvent("P4ssword"));
+      button = container.querySelector("button");
+
+      return rendered;
     };
 
     it("sets the username value into state", () => {
@@ -56,6 +71,110 @@ describe("LoginPage", () => {
       const passwordInput = queryByPlaceholderText("Your password");
       fireEvent.change(passwordInput, changeEvent("P4ssword"));
       expect(passwordInput).toHaveValue("P4ssword");
+    });
+
+    it("calls postLogin when the actions are provided in props and input fields have value", () => {
+      const actions = {
+        postLogin: jest.fn().mockResolvedValue({}),
+      };
+      setupForSubmit({ actions });
+      fireEvent.click(button);
+      expect(actions.postLogin).toHaveBeenCalledTimes(1);
+    });
+
+    it("does not throw exception when clicking the button when actions not provided in props", () => {
+      setupForSubmit();
+      expect(() => fireEvent.click(button)).not.toThrow();
+    });
+
+    it("calls postLogin with credentials in body", () => {
+      const actions = {
+        postLogin: jest.fn().mockResolvedValue({}),
+      };
+      setupForSubmit({ actions });
+      fireEvent.click(button);
+
+      const expectedUserObject = {
+        username: "my-user-name",
+        password: "P4ssword",
+      };
+
+      expect(actions.postLogin).toBeCalledWith(expectedUserObject);
+    });
+
+    it("enables the button when username and password is not empty", () => {
+      setupForSubmit();
+      expect(button).not.toBeDisabled();
+    });
+
+    it("disables the button when username is empty", () => {
+      setupForSubmit();
+      fireEvent.change(usernameInput, changeEvent(""));
+      expect(button).toBeDisabled();
+    });
+
+    it("disables the button when password is empty", () => {
+      setupForSubmit();
+      fireEvent.change(passwordInput, changeEvent(""));
+      expect(button).toBeDisabled();
+    });
+
+    it("displays alert when login fails", async () => {
+      const actions = {
+        postLogin: jest.fn().mockRejectedValue({
+          response: {
+            data: {
+              message: "Login failed",
+            },
+          },
+        }),
+      };
+      const { queryByText } = setupForSubmit({ actions });
+      fireEvent.click(button);
+
+      await waitFor(() => queryByText("Login failed"));
+      const alert = queryByText("Login failed");
+      expect(alert).toBeInTheDocument();
+    });
+
+    it("clears alert when user changes username", async () => {
+      const actions = {
+        postLogin: jest.fn().mockRejectedValue({
+          response: {
+            data: {
+              message: "Login failed",
+            },
+          },
+        }),
+      };
+      const { queryByText } = setupForSubmit({ actions });
+      fireEvent.click(button);
+
+      await waitFor(() => queryByText("Login failed"));
+      fireEvent.change(usernameInput, changeEvent("updated-username"));
+
+      const alert = queryByText("Login failed");
+      expect(alert).not.toBeInTheDocument();
+    });
+
+    it("clears alert when user changes password", async () => {
+      const actions = {
+        postLogin: jest.fn().mockRejectedValue({
+          response: {
+            data: {
+              message: "Login failed",
+            },
+          },
+        }),
+      };
+      const { queryByText } = setupForSubmit({ actions });
+      fireEvent.click(button);
+
+      await waitFor(() => queryByText("Login failed"));
+      fireEvent.change(passwordInput, changeEvent("updated-P4ssword"));
+
+      const alert = queryByText("Login failed");
+      expect(alert).not.toBeInTheDocument();
     });
   });
 });
