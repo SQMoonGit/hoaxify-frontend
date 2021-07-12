c19a66703d1a0ca373dd3bb2d13259fe83180c4b
SQMoonGit <hykmoon@gmail.com>
2021-07-12 11:34:14 -0500

    Added input changes

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,18 +2,44 @@ import React from "react";
 import Input from "../components/input";
 
 export class LoginPage extends React.Component {
+  state = {
+    username: "",
+    password: "",
+  };
+
+  onChangeUsername = (event) => {
+    const value = event.target.value;
+    this.setState({
+      username: value,
+    });
+  };
+
+  onChangePassword = (event) => {
+    const value = event.target.value;
+    this.setState({
+      password: value,
+    });
+  };
+
   render() {
     return (
       <div className="container">
         <h1 className="text-center">Login</h1>
         <div className="col-12 mb-3">
-          <Input label="Username" placeholder="Your username"></Input>
+          <Input
+            label="Username"
+            placeholder="Your username"
+            value={this.state.username}
+            onChange={this.onChangeUsername}
+          ></Input>
         </div>
         <div className="col-12 mb-3">
           <Input
             label="Password"
             type="password"
             placeholder="Your password"
+            value={this.state.password}
+            onChange={this.onChangePassword}
           ></Input>
         </div>
         <div className="text-center">
